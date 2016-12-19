864de5356f70b53a92dc0c03a3fb584da3faea21
SamiKhan <sami.khan9892@gmail.com>
2016-12-19 20:11:54 +0500

    Print function added in member class (Sami ur Rehman Khan)

--- Team_Assignment_GITHUB/src/Team_Work/Sami_Ur_Rehman_Khan.java
+++ Team_Assignment_GITHUB/src/Team_Work/Sami_Ur_Rehman_Khan.java
@@ -1,5 +1,9 @@
 package Team_Work;
 
 public class Sami_Ur_Rehman_Khan {
+	public void print() {
+		System.out.println("Team member Name  : Sami ur Rehman Khan");
+		System.out.println("Team member RegNo : FA13 - BSE - 132");
+	}
 
 }
